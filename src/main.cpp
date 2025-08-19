dc9f81eb0651266ff7e0369f2eb3fc64e98b2954
Amandeep <2023kucp1077@iiitkota.ac.in>
2025-08-19 22:49:59 +0530

    added plant spawning and near death animals tint

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,16 @@
 #include <thread>
 #include <chrono>
 #include <ctime>
+#include <SFML/Graphics.hpp>
 #include "../include/World.h"
 #include "../include/Entity.h"
 #include "../include/AnimalConfig.h"
 
 using namespace std;
 
-const uint32_t WIDTH = 10;
-const uint32_t HEIGHT = 10;
-
-void clearScreen(){
-    #ifdef _WIN32
-        system("cls");
-    #else
-        system("clear");
-    #endif
-}
-
-void sleepMainThread(int milliseconds){
-    this_thread::sleep_for(chrono::milliseconds(milliseconds));
-}
+const uint32_t WIDTH = 15;
+const uint32_t HEIGHT = 15;
+const uint32_t TILE_SIZE = 48;
 
 void initializeWorldWithEntities(const uint32_t& numPlants, const uint32_t& numHerbivores, const uint32_t& numCarnivores){
     World &world = World::getInstance();
@@ -36,17 +26,44 @@ void initializeWorldWithEntities(const uint32_t& numPlants, const uint32_t& numH
         world.addEntityType(animalconfig::CARNIVORE_CONFIG.symbol);
     }
 }
-
 int main(){
     srand(time(0));
     World &world = World::getInstance();
     world.initialize(HEIGHT, WIDTH);
 
-    initializeWorldWithEntities(5, 3, 2);
+    initializeWorldWithEntities(15, 10, 2);
+
+    sf::RenderWindow window(sf::VideoMode(WIDTH * TILE_SIZE, HEIGHT * TILE_SIZE), "EcoSim");
+
+    std::map<char, sf::Texture> textures;
+    if (!textures['H'].loadFromFile("../sprites/sheep.png")) { cerr << "Error loading sheep.png" << endl; return -1; }
+    if (!textures['C'].loadFromFile("../sprites/wolf.png")) { cerr << "Error loading wolf.png" << endl; return -1; }
+    if (!textures['*'].loadFromFile("../sprites/bush.png")) { cerr << "Error loading bush.png" << endl; return -1; }
+    if (!textures['.'].loadFromFile("../sprites/tile.png")) { cerr << "Error loading tile.png" << endl; return -1; }
+    textures[' '] = textures['.'];
+
     int counter = 0;
-    while(true){
+    uint32_t spawnTime = 0;
+    while(window.isOpen()){
+        if(spawnTime == 0){
+            spawnTime = (rand() % 4) + 4;
+        } else if(spawnTime == (counter % 10)){
+            world.addEntityType(animalconfig::PLANT_CONFIG.symbol);
+            spawnTime = 0;
+        }
+        sf::Event event;
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
         world.run();
-        world.displayWorld();
+        
+        window.clear();
+        // Pass the TILE_SIZE to the render function
+        world.render(window, textures, TILE_SIZE);
+        window.display();
+
         cout << "Iteration: " << counter++ << " - Occupied Cells: " << world.getOccupiedCellsCount() << endl;
         
         if (world.getOccupiedCellsCount() == 0) {
@@ -54,8 +71,7 @@ int main(){
             break;
         }
         
-        sleepMainThread(800);
-        clearScreen();
+        this_thread::sleep_for(chrono::milliseconds(400));
     }
     return 0;
 }
